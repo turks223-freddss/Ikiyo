decaff08a5af1254f150120f9517a9116a312e0e
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2025-05-06 10:58:57 +0800

    icons

---
+++ assets/images/homeIcons/index.js
@@ -0,0 +1,8 @@
+export { default as AvatarIcon } from './avatar.png';
+export { default as TaskIcon } from './task.png';
+export { default as EditRoomIcon } from './edit_room.png';
+export { default as FriendlistIcon } from './friendlist.png';
+export { default as MapsIcon } from './maps.png';
+export { default as ShopIcon } from './shop.png';
+export { default as HeartIcon } from './hearts.png';
+export { default as IkicoinIcon } from './ikicoin.png';
