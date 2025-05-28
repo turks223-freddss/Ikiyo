a452380fbde1647b806ddbb3a7287a4a4da7668a
Porings . <kylemanayon@gmail.com>
2025-05-28 22:21:40 +0800

    add placeholder inventory and spawning of items

--- app/assets/RoomComponents/Room.tsx
+++ app/assets/RoomComponents/Room.tsx
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { View, Image, StyleSheet, Dimensions } from 'react-native';
 import Draggable from './Draggable';
+import RoomInventory from './RoomInventory';
+import eventBus from "../utils/eventBus";
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
 
+interface Item {
+  id: number;
+  x: number;
+  y: number;
+  itemDimensions: {
+    width: number;
+    height: number;
+  }
+  state?: string;
+}
+
 const initialItems = [
-  { id: 1, x: screenWidth/20*3, y: screenHeight/10*4, itemDimensions: {width: 1, height: 1}, state: 'floor'},
+  { id: 1, x: screenWidth/20*3, y: screenHeight/10*4, itemDimensions: {width: 2, height: 1}, state: 'floor'},
   { id: 2, x: screenWidth/20*4, y: screenHeight/10*5, itemDimensions: {width: 1, height: 1}, state: 'wall' },
   { id: 3, x: screenWidth/20*5, y: screenHeight/10*6, itemDimensions: {width: 1, height: 1} },
   { id: 4, x: screenWidth/20*6, y: screenHeight/10*7, itemDimensions: {width: 1, height: 1} },
@@ -16,9 +29,7 @@ const Grid: React.FC = () => {
   const gridSize = { width: 1, height: 1};
   const gridDimensions = { width: screenWidth, height: screenHeight};
   const [items, setItems] = useState(initialItems);
-
-  useEffect(() => {
-    }, [items]);
+  const [isInventoryOpen, setIsInventoryOpen] = useState(false);
 
   // Handle position update after item is dropped
   const updateItemPosition = (id: number, newPosition: { x: number; y: number }) => {
@@ -29,19 +40,45 @@ const Grid: React.FC = () => {
     );
   };
 
+  useEffect(() => {
+    const addNewItem = (item: Item) => {
+      setItems(prevItems => [
+        ...prevItems,
+        item
+      ]);
+      eventBus.emit("closeInventory")
+    }
+
+    eventBus.on("newItem", addNewItem);
+    
+    return () => {
+      eventBus.off("newItem", addNewItem);
+    }
+  });
+
+  useEffect(() => {
+    const handleOpenInventory = () => {
+      setIsInventoryOpen(true);
+    };
+
+    const handleCloseInventory = () => {
+      setIsInventoryOpen(false);
+    }
+
+    eventBus.on("openInventory", handleOpenInventory);
+    eventBus.on("closeInventory", handleCloseInventory);
+
+    return () => {
+      eventBus.off("openInventory", handleOpenInventory);
+      eventBus.off("closeInventory", handleCloseInventory);
+    };
+  }, []);
+
   const updateCellSize = (cellSize: {width: number, height: number},
       itemDimensions: {width: number, height: number}) => {
         return {
           width: Math.round(cellSize.width * itemDimensions.width), 
           height: Math.round(cellSize.height * itemDimensions.height)
-        }
-  }
-
-  const updateGridSize = (gridSize: {width: number, height: number},
-      itemDimensions: {width: number, height: number}) => {
-        return {
-          width: Math.round(gridSize.width / itemDimensions.width), 
-          height: Math.round(gridSize.height / itemDimensions.height)
         }
   }
   
@@ -62,6 +99,11 @@ const Grid: React.FC = () => {
           onDrop={(newPosition) => updateItemPosition(item.id, newPosition)} 
         />
       ))}
+      {isInventoryOpen && (
+        <View style={styles.inventory}>
+          <RoomInventory/>
+        </View>
+      )}
     </View>
   );
 };
@@ -73,6 +115,13 @@ const styles = StyleSheet.create({
     width: screenWidth,
     height: screenHeight,
     backgroundColor: '#000000',
+  },
+  
+  inventory: {
+    position: 'absolute',
+    margin: 20,
+    width: screenWidth*.3,
+    height: screenHeight*.8
   },
 
   wall: {
