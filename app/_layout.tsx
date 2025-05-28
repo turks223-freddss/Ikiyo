93111967cc05c16b6a2f64346d62ae6cf9bfc2ac
Li~chan <137862600+ChiakiLi-chan@users.noreply.github.com>
2025-05-28 20:46:05 +0800

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,27 @@
 import { Stack, useRouter } from "expo-router";
 import React from "react";
 import { useEffect } from "react";
+import { MusicProvider } from "./assets/utils/musiccontext";
 
 export default function RootLayout() {
   const router = useRouter();
 
 
   useEffect(() => {
-    router.replace("/loading"); // ✅ Always start at the loading screen
+    router.replace("/school"); // ✅ Always start at the loading screen
   }, []);
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="loading" options={{ headerShown: false }} />
-      <Stack.Screen name="login" options={{ headerShown: false }} />
-      <Stack.Screen name="signup" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(school_tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="community" options={{ headerShown: false }} />
-      <Stack.Screen name="profile" options={{ headerShown: false }} />
-    </Stack>
+    <MusicProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="loading" options={{ headerShown: false }} />
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(school_tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="community" options={{ headerShown: false }} />
+        <Stack.Screen name="profile" options={{ headerShown: false }} />
+      </Stack>
+    </MusicProvider>
   );
 }
